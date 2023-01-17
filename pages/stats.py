a0fe0475457d653ca32d92b0f0f87bfc14daa816
Felix <felix.mars@outlook.com>
2023-01-17 19:50:51 +0000

    share data

--- pages/stats.py
+++ pages/stats.py
@@ -3,11 +3,11 @@ from dash import dcc,html
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
+import app
 
 dash.register_page(__name__,path="/stats")
 
-data = pd.read_csv('./example_files/running-example.csv', sep=';')
-
+data = app.data
 fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
 
 
@@ -16,8 +16,9 @@ fig2 = px.scatter(df, x="sepal_width", y="sepal_length")
 
 layout = html.Div([
     html.H1('Stats'),
+    html.Hr(),
     html.H2('Week View'),
-
     dcc.Graph(figure=fig),
+    html.Hr(),
     html.H2('Month View'),
     dcc.Graph(figure=fig2)])
