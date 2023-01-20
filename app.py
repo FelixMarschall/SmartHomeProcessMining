928435be878e5ddf608576d14aeb19d286063586
Felix <felix.mars@outlook.com>
2023-01-20 23:37:11 +0000

    file oupload works

--- app.py
+++ app.py
@@ -3,22 +3,28 @@ import datetime
 import io
 import time
 import pm4py
+import logging
 
 import dash
 from dash.dependencies import Input, Output, State
 from dash import dcc, html, dash_table
+from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 
 import pandas as pd
 
 BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-table_example = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
 # data = pd.read_csv('./example_files/running-example.csv', sep=';')
-log = pm4py.read_xes('./example_files/running-example.xes')
+example_log = pm4py.read_xes('./assets/running-example.xes')
+log = None
+print("New init")
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages=True)
 
+# set up logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 sidebar = dbc.Nav(
     [
@@ -41,59 +47,66 @@ app.layout = dbc.Container(
         dash.page_container,
         ]))
 
-def parse_contents(contents, filename, date):
-    """Parse a dash upload component contents."""
-    content_type, content_string = contents.split(';')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            df.to_dict('records')
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
-
 ### Upload File Box
-@app.callback(Output('output-data-upload', 'children'),
+@app.callback(Output('data-table', 'children'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'),
-              prevent_initial_call=True)
-def update_output(list_of_contents, list_of_names, list_of_dates):
+              prevent_initial_call=False)
+def update_output(contents, list_of_names, list_of_dates):
     """Called when file is uploaded."""
-    print("Callback 'File upload called'")
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
+    logger.debug("Callback 'File upload called'")
 
+    global log
+    if log is None and contents is None:
+        logger.debug("No log found, using example log")
+        return html.Div(id ="data-table", children=[
+        html.Div(id="info-field", children='Example log is displayed below.'),
+        dash_table.DataTable(example_log.to_dict('records'),
+        columns= [{"name": i, "id": i} for i in example_log.columns],
+        filter_action='native',
+        page_action='none',
+        style_table={'height': '900px', 'overflowY': 'auto'})
+    ]),
+    elif contents is None:
+        return dash_table.DataTable(log.to_dict('records'),
+        columns= [{"name": i, "id": i} for i in log.columns],
+        filter_action='native',
+        page_action='none',
+        style_table={'height': '900px', 'overflowY': 'auto'})
+
+    CSV_SEPERATOR = ','
+    content_type, content_string = contents.split(CSV_SEPERATOR)
+    decoded = base64.b64decode(content_string)
+
+    logger.info(f"File uploaded with name: '{list_of_names}' and date: {list_of_dates}")
+    
+    try:
+        if list_of_names.endswith('.csv'):
+            UPLOAD_PATH = "assets/temp/uploaded.csv"
+            f = open(UPLOAD_PATH, "w")
+            f.write(decoded.decode('utf-8'))
+            f.close()
+            log = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            logger.debug("upload file is csv log")
+        elif list_of_names.endswith('.xes'):
+            UPLOAD_PATH = "assets/temp/uploaded.xes"
+            f = open(UPLOAD_PATH, "w")
+            f.write(decoded.decode('utf-8'))
+            f.close()
+            log = pm4py.read_xes(UPLOAD_PATH)
+            logger.debug("upload file is xes log")
+    except Exception as e:
+        logger.error(type(e).__name__ + " while reading file: " + str(e))
+        raise PreventUpdate()
+
+    return dash_table.DataTable(log.to_dict('records'),
+        columns= [{"name": i, "id": i} for i in log.columns],
+        filter_action='native',
+        page_action='none',
+        style_table={'height': '900px', 'overflowY': 'auto'})
+
+    #return log.to_json(date_format='iso', orient='split')
 
 ### Transformation "start mining" Button
 @app.callback(
@@ -109,7 +122,10 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
 )
 def update_transformation(value, algo, graph):
     """Calles when transformation button is clicked."""
-    print("Callback 'start mining' button with value:", value, "and algo:", algo, "and graph:", graph)
+    logger.debug(f"Callback 'start mining' button with value: {value} and algo: {algo} and graph: {graph}")
+
+    # if log == None:
+        # log = example_log
 
     start_time = time.perf_counter()
 
@@ -120,11 +136,13 @@ def update_transformation(value, algo, graph):
     elif algo == "heuristic":
         process_model, start, end = pm4py.discover_petri_net_heuristics(log)
     else:
+        logger.error("Algorithm not supported")
         raise ValueError("Algorithm not supported")
 
     mining_duration = time.perf_counter() - start_time
     mining_duration = "mining duration: "+ str(round(mining_duration,2)) + "s"
-    
+    logger.info("Duration of mining: " + mining_duration)
+
     pt = pm4py.convert_to_process_tree(process_model, start, end)
     bpmn = pm4py.convert_to_bpmn(process_model, start, end)
 
@@ -135,4 +153,4 @@ def update_transformation(value, algo, graph):
     return dash.get_asset_url("bpmn.svg"), dash.get_asset_url("pn.svg"), dash.get_asset_url("pt.png"),mining_duration, None
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
--- pages/data.py
+++ pages/data.py
@@ -1,10 +1,12 @@
 import dash
 from dash import dcc,html, dash_table
 import pandas as pd
+import pm4py
 
 dash.register_page(__name__,path="/data")
 
-df = pd.read_csv('./example_files/running-example.csv', sep=';')
+#df = pm4py.read_xes('./assets/running-example.xes')
+
 
 layout = html.Div([
     html.H1('Data'),
@@ -27,11 +29,7 @@ layout = html.Div([
                     'margin': '10px'
                 }),
     ### Table
-    html.Div(id="info-field", children='Your event log is displayed below.'),
-    html.Div(id="output-data-upload", children=[]),
-    dash_table.DataTable(df.to_dict('records'),
-        columns= [{"name": i, "id": i} for i in df.columns],
-        filter_action='native',
-        page_action='none',
-        style_table={'height': '900px', 'overflowY': 'auto'}),
+    html.Div(id ="data-table", children=[
+        html.Div(id="info-field", children='Loading...'),
+    ]),
 ])
