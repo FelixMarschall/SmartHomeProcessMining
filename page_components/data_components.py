0a1cc2501fb29c4173f088eb622ac8b8a3796e97
Felix <felix.mars@outlook.com>
2023-01-21 14:29:18 +0000

    define return type

--- page_components/data_components.py
+++ page_components/data_components.py
@@ -2,7 +2,7 @@ import pandas as pd
 from dash import dcc, html, dash_table
 
 
-def get_data_table(log: pd.DataFrame):
+def get_data_table(log: pd.DataFrame) -> dash_table.DataTable:
    return dash_table.DataTable(log.to_dict('records'),
         columns= [{"name": i, "id": i} for i in log.columns],
         filter_action='native',
