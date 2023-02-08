48f60a5aa7c5ba53d324a57c6c4518dec4b8f59f
Felix <felix.mars@outlook.com>
2023-02-08 22:30:29 +0000

    edit

--- setup.py
+++ setup.py
@@ -6,4 +6,16 @@ setup(
     author = "Felix Marschall",
     author_email = "felix.marschall@student.kit.edu",
     packages=find_packages(),
+    install_requires=[
+        "dash",
+        "dash-bootstrap-components",
+        "dash-core-components",
+        "dash-html-components",
+        "dash-renderer",
+        "dash-table",
+        "pm4py",
+        "pandas",
+        "plotly",
+        "Pillow",
+    ]
 )
