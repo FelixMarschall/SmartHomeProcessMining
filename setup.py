d23d0167186f011dd4d41b087e0f79f4d937d2e0
Felix <felix.mars@outlook.com>
2023-02-08 21:08:44 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "proocess_management_smart_home",
+    version = "1.0.0",
+    author = "Felix Marschall",
+    author_email = "felix.marschall@student.kit.edu",
+    packages=find_packages(),
+)
