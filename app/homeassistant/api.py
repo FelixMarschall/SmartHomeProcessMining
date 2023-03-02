b6e5160ccc86ab36bec8eb1598c61373f1375b01
Felix <felix.mars@outlook.com>
2023-03-02 19:47:28 +0000

    add API

--- app/homeassistant/api.py
+++ app/homeassistant/api.py
@@ -1,13 +1,50 @@
 import requests
+import pandas as pd
+import logging
+
+
+internal_host = "http://supervisor/core/api/"
+host = "homeassistant.local"
+port = 8123
+timestamp = "2022-02-15T00:00:00+02:00"
+
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIzMzc5NDBmZmZlNWU0MWJiYmY4MDZmYzYzZmM0MjNhNSIsImlhdCI6MTY3NzcwNjg4MSwiZXhwIjoxOTkzMDY2ODgxfQ.IrgtgxwY1dKjTcNV59ZhX-URvptsQ_MmE6XsPN-23WA"
+
 
 class Api:
+    is_ha_env = False
+
     def __init__(self) -> None:
         # running on home assistant environment?
         pass
     
-    def hello():
-        return 'Hello'
+    def ping(self):
+        headers_auto_config = {
+            "Authorization": 'Bearer ${SUPERVISOR_TOKEN}',
+            "content-type": "application/json",
+        }
 
-    def get_logbook():
+        global is_ha_env
+        try:
+            response = requests.get(internal_host, headers=headers_auto_config)
+            is_ha_env = True
+        except requests.exceptions.ConnectionError as e:
+            is_ha_env = False
+            logging.error(f"API Logbook request failed with Supervisor Token (auto auth)")
+
+        if not is_ha_env:
+            url = f"http://{host}:{port}/api/"
+            headers = {
+                "Authorization": 'Bearer ' + token,
+                "content-type": "application/json",
+            }
+            response = requests.get(url, headers=headers)
+
+            if response.status_code < 200 or response.status_code > 202:
+                logging.error(f"API Logbook request failed with personal token: {response.status_code}")
+                raise requests.exceptions.ConnectionError("Cannot connect to Homeassistant API")
+        return response.text
+
+    def get_logbook(self):
         '''Returns the logbook'''
         pass    
--- app/pages/home.py
+++ app/pages/home.py
@@ -1,4 +1,5 @@
 import dash
+from homeassistant import Api
 from dash import dcc, html
 
 dash.register_page(__name__,path="/")
@@ -7,6 +8,7 @@ dash.register_page(__name__,path="/")
 layout = html.Div([
     html.H1('Process Mining with Smart Home Data'),
     html.Div('Import your first log and start with process mining!'),
+    html.Div(Api().ping),
     html.Hr(),
     html.H2('What can I do here?'),
     html.Div('This is a demo of the process mining capabilities of the Smart Home Data Analytics Platform. You can import your own logs and start with process mining. You can also use the example logs provided in the example files folder.'),
